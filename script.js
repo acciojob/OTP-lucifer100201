c91adff75ad9409fe4b091e190e82c7f0e674abc
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-03-21 06:22:14 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-  let inputs= document.querySelectorAll(".code");
-
-
-
+let inputs= document.querySelectorAll(".code");
 
 inputs.forEach((input,index)=>{
-	input.addEventListener("input",(e)=>{
-		console.log(e.target.value.length)
-		if(e.target.value.length===1){
-			if(index < inputs.length - 1){
-				inputs[index+1].focus();
-			}
-		}
-	})
-	input.addEventListener("keydown",(e)=>{
-	if(e.key=== 'Backspace' && input.value.length === 0){
-		if(index >0){
-			inputs[index - 1].focus();
-		}
-	}
-})
+    input.addEventListener("input",(e)=>{
+        console.log(e.target.value.length)
+        if(e.target.value.length===1){
+            if(index < inputs.length - 1){
+                inputs[index+1].focus();
+            }
+        }
+    })
+    input.addEventListener("keydown",(e)=>{
+        if(e.key=== 'Backspace' && input.value.length === 0){
+            if(index >0){
+                inputs[index - 1].focus();
+            }
+        }
+    })
 });
-
-
