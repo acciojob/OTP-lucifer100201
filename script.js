27ae6666a4773509e149d122448e0469bfd2dcc0
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-03-29 06:43:04 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
+let inputs = document.querySelectorAll(".code");
 
-
-  let inputs = document.querySelectorAll(".code");
-
-  inputs.forEach((input, index) => {
-
-    input.addEventListener("input", (e) => {
-
-      // console.log(e.target.value.length);
-
-      if (e.target.value.length === 1) {
-
-        if (index < inputs.length - 1) {
-
-          inputs[index + 1].focus();
-
-        }
-
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    if (e.target.value.length === 1) {
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
       }
-
-    });
-
-    input.addEventListener("keydown", (e) => {
-
-      if (e.key === 'Backspace' && input.value.length === 0) {
-
-        if (index > 0) {
-
-          inputs[index - 1].focus();
-
-        }
-
-      }
-
-    });
-
+    }
   });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === 'Backspace' && input.value.length === 0) {
+      if (index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+  });
+});
+
+// Add this line to focus on the first input initially
+inputs[0].focus();
